2f4622a321360876ec3b4d6d74fb2084852b1ece
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-08-18 10:50:42 +0000

    add comments to the script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//when the complete DOM is loaded, event listener is added to place 16*16 smaller grids inside the bigger grid
+//when the complete DOM is loaded, event listener places 16*16 smaller grids inside the bigger grid
 document.addEventListener("DOMContentLoaded", function(){
-    const bigGrid = document.querySelector(".grid");
+    const bigGrid = document.querySelector(".canvas");
     const squares = 16 * 16;
     for(let i = 1; i <= squares; i++){
         const smallGrid = document.createElement("div");
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 });
 
+//when input[type = range] is changed, the squares inside canvas change and the changed size is displayed
 function createGrid(){
     const range = document.querySelector("#slide");
-    const bigGrid = document.querySelector(".grid");
+    const bigGrid = document.querySelector(".canvas");
     const slider = document.querySelector(".size");
     const num = range.value;
     const squares = num * num;
@@ -20,7 +21,8 @@ function createGrid(){
     slider.innerText = `${num} x ${num}`;
     for(let i = 1; i <= squares; i++){
         const smallGrid = document.createElement("div");
-        smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px; box-sizing: border-box; padding: 0; margin: 0; background-color: transparent; border: 1px solid rgba(0, 0, 0, 0.701);`;
+        smallGrid.classList.add("small16");
+        smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px;`
         bigGrid.appendChild(smallGrid);
     }
 }
