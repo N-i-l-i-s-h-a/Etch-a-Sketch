5363f1fb2c825a10d2709d7ebb3ae86a505dc480
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-08-19 00:32:10 +0000

    add eventListeners that trigger when smaller grids are clicked or dblclicked to fill color

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //when the complete DOM is loaded, event listener places 16*16 smaller grids inside the bigger grid
 document.addEventListener("DOMContentLoaded", function(){
+    const colorIn = document.querySelector("#col");
+    let colorCh = colorIn.value;
     const bigGrid = document.querySelector(".canvas");
     const squares = 16 * 16;
     for(let i = 1; i <= squares; i++){
         const smallGrid = document.createElement("div");
-        smallGrid.classList.add("small16");
+        smallGrid.classList.add("small");
+        smallGrid.addEventListener('click', function(){
+            smallGrid.style.cssText = `background-color : ${colorCh}`
+            smallGrid.addEventListener('mousedown', hoverFill());
+        });
         bigGrid.appendChild(smallGrid);
     }
 });
 
 //when input[type = range] is changed, the squares inside canvas change and the changed size is displayed
 function createGrid(){
+    const colorIn = document.querySelector("#col");
+    let colorCh = colorIn.value;
     const range = document.querySelector("#slide");
     const bigGrid = document.querySelector(".canvas");
     const slider = document.querySelector(".size");
@@ -21,8 +29,16 @@ function createGrid(){
     slider.innerText = `${num} x ${num}`;
     for(let i = 1; i <= squares; i++){
         const smallGrid = document.createElement("div");
-        smallGrid.classList.add("small16");
+        smallGrid.classList.add("small");
         smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px;`
+        smallGrid.addEventListener('click', function(){
+            smallGrid.classList.add("small");
+            smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px; background-color : ${colorCh}`
+        });
+        smallGrid.addEventListener('dblclick', function(){
+            const nodeList = document.querySelectorAll(".small");
+            nodeList.forEach(node => {  node.addEventListener('mouseenter', () => node.style.cssText = `width: ${dim}px; height: ${dim}px; background-color: ${colorCh}`)   });
+        });
         bigGrid.appendChild(smallGrid);
     }
 }
