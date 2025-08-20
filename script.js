16cb0b1b9412a1336c540cad13d5084ce9a423af
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-08-20 19:10:59 +0000

    change reset button img and add functionality to eraser and reset buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //when the complete DOM is loaded, event listener places 16*16 smaller grids inside the bigger grid
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener('DOMContentLoaded', function(){
     const bigGrid = document.querySelector(".canvas");
     const squares = 16 * 16;
     for(let i = 1; i <= squares; i++){
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     return draw(31);
 });
+
+//after erase button is clicked, the state isErasing is set true
+let isErasing = false;
 
 //global state that determines when the mouseenter event triggers coloring
 let isDrawing = false;
@@ -35,12 +38,13 @@ function createGrid(){
 //draw function that colors small grids either when the mousedown event occurs or when the mouse hovers over the small grids after some grid was dblclicked somewhere 
 function draw(dim){
     const colorIn = document.querySelector("#col");
-    let colorCh = colorIn.value;
+    let colorCh = colorIn.value;   
     colorIn.addEventListener('change', (e) => colorCh = e.target.value);
     const bigGrid = document.querySelector(".canvas");
     bigGrid.addEventListener('mousedown', () => isDrawing = true);
-    bigGrid.addEventListener('mouseup', () => isDrawing = false);
-    const smallGrids = document.querySelectorAll(".small");
+    bigGrid.addEventListener('mouseleave', () => isDrawing = false);
+    document.addEventListener('mouseup', () => isDrawing = false);
+    const smallGrids = document.querySelectorAll(".small"); 
     smallGrids.forEach(smallGrid => 
         {   
             smallGrid.addEventListener('mouseenter', function(){
@@ -56,4 +60,36 @@ function draw(dim){
             });
         }            
     );       
+}
+
+
+function eraser(){
+    const sq = (document.querySelector("#slide")).value;
+    const dim = 496 / sq;
+    const bigGrid = document.querySelector(".canvas");
+    let bgcol = bigGrid.style.backgroundColor;
+    bigGrid.addEventListener('mousedown', () => isErasing = true);
+    bigGrid.addEventListener('mouseleave', () => isErasing = false);
+    bigGrid.addEventListener('mouseup', () => isErasing = false);
+    const smallGrids = document.querySelectorAll(".small"); 
+    smallGrids.forEach(smallGrid => 
+        {   
+            smallGrid.addEventListener('mouseenter', function(){
+                if(isErasing){
+                    smallGrid.classList.add("small");
+                    smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px;`
+                    smallGrid.style.backgroundColor =  `${bgcol}`;
+            }});
+            smallGrid.addEventListener('mousedown', function(){
+                smallGrid.classList.add("small");
+                smallGrid.style.cssText = `width: ${dim}px; height: ${dim}px;`
+                smallGrid.style.backgroundColor =  `${bgcol}`;
+            });
+        }            
+    ); 
+    const colorIn = document.querySelector("#col");
+    colorIn.addEventListener('click', function(){
+        isErasing = false;
+        draw(dim);
+    });
 }
